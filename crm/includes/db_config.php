2c9fa09aeea0fa5cd2fa3ad2f68b06a58708cbd4
prabhuvajjiram <36006354+prabhuvajjiram@users.noreply.github.com>
2024-12-14 00:20:05 -0500

    Update db_config.php

--- crm/includes/db_config.php
+++ crm/includes/db_config.php
@@ -1,7 +1,7 @@
 <?php
 // Production database credentials
-define('DB_HOST', '103.14.122.124');
-define('DB_USER', 'theangel_theangel');
-define('DB_PASS', '#QSX$V032uXY');
+define('DB_HOST', '103.');
+define('DB_USER', 't');
+define('DB_PASS', '#');
 define('DB_NAME', 'theangel_quotes');
 ?>
